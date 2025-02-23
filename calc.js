7802e853800e3ce9af752b8b36f9cbcbbb2372fd
davidchavezc <davidchavezc05@gmail.com>
2025-02-23 13:35:50 -0600

    Rename operate.js into calc.js

---
+++ calc.js
@@ -0,0 +1,12 @@
+export function calc(num1, num2, operator){
+    switch(operator){
+        case 'sum':
+            return num1+num2;
+        case 'rest':
+            return num1-num2;
+        case 'multiply':
+            return num1*num2;
+        case 'divide':
+            return num1/num2;
+    }
+}
